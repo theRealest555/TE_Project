d8eab64cffe44722cb630aeba10d30a755f6e104
youness ajdir <younessajdir44@gmail.com>
2025-05-13 21:06:59 +0100

    fix

--- Controllers/SubmissionsController.cs
+++ Controllers/SubmissionsController.cs
@@ -12,7 +12,7 @@ namespace TE_Project.Controllers
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    // Removed [Authorize] from here
     [Produces("application/json")]
     public class SubmissionsController : ControllerBase
     {
@@ -37,24 +37,14 @@ namespace TE_Project.Controllers
         /// <returns>Created submission ID</returns>
         /// <response code="201">Returns the newly created submission ID</response>
         /// <response code="400">If the submission data is invalid</response>
-        /// <response code="401">If the user is not authenticated</response>
-        /// <response code="403">If the user doesn't have access to the specified plant</response>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        // Removed [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        // Removed [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<IActionResult> Create([FromForm] SubmissionDto model)
         {
-            // Verify the user has access to this plant
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (string.IsNullOrEmpty(userId))
-                return Unauthorized(new { message = "User identity not found" });
-
-            var hasAccess = await _authService.ValidateAdminAccessToPlantAsync(userId, model.PlantId);
-            if (!hasAccess)
-                return Forbid();
-
+            // Removed user validation logic since submissions are now public
             try
             {
                 var submission = await _submissionService.CreateSubmissionAsync(model);
@@ -86,6 +76,7 @@ namespace TE_Project.Controllers
         /// <response code="403">If the user doesn't have access to the submission's plant</response>
         /// <response code="404">If the submission is not found</response>
         [HttpGet("{id}")]
+        [Authorize] // Added authorization here
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
@@ -113,6 +104,7 @@ namespace TE_Project.Controllers
         }
 
         [HttpGet("plant/{plantId}")]
+        [Authorize] // Added authorization here
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
@@ -134,7 +126,6 @@ namespace TE_Project.Controllers
             return Ok(submissions);
         }
 
-
         /// <summary>
         /// Gets all submissions (Super Admin only)
         /// </summary>
@@ -143,7 +134,7 @@ namespace TE_Project.Controllers
         /// <response code="401">If the user is not authenticated</response>
         /// <response code="403">If the user is not a Super Admin</response>
         [HttpGet]
-        [Authorize(Roles = AdminRole.SuperAdmin)]
+        [Authorize(Roles = AdminRole.SuperAdmin)]  // This already had role-specific authorization
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
